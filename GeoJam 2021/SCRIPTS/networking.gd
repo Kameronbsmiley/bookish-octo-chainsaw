7197af9e23c8d434e6be46e32d6c6606230e8206
Nico <ncollie42@gmail.com>
2021-07-22 21:20:22 -0700

    Switched from using Enet to websockets so everything should work on itch.io

--- GeoJam 2021/SCRIPTS/networking.gd
+++ GeoJam 2021/SCRIPTS/networking.gd
@@ -5,39 +5,51 @@ var SERVER_PORT = 4242
 var LINODE = "173.255.248.250"  #Make Environtment var?
 var SERVER_IP = LINODE
 var LOCAL_HOST = "127.0.0.1"
+var peer
 
-remote var top_scores = [{"N": "Nico", "T": 209}]
+remotesync var top_scores = [{"N": "Nico", "T": 209}]
 
 func _ready():
 	print("args:", OS.get_cmdline_args())
 	var is_server = false
 	if "server" in OS.get_cmdline_args():
 		is_server = true
-		print("Starting as server")
 		
 	init_connection(is_server)
 	get_tree().connect("network_peer_connected", self, "_player_connected")
+	if is_server:
+		print("is listening: ", peer.is_listening())
 	
 func init_connection(is_server: bool):
-	var peer = NetworkedMultiplayerENet.new()
 	if is_server:
-		peer.create_server(SERVER_PORT)
+		print("Starting as server")
+		peer = WebSocketServer.new()  #NetworkedMultiplayerENet
+		peer.listen(SERVER_PORT, PoolStringArray(), true)
+#		peer.create_server(SERVER_PORT)
 	else:
+		print("Starting as client")
+		peer = WebSocketClient.new()
 		if "local" in OS.get_cmdline_args():
+			print("Using localhost")
 			SERVER_IP = LOCAL_HOST
-		peer.create_client(SERVER_IP,SERVER_PORT)
+		var url = "ws://" + (SERVER_IP) + ":" + str(SERVER_PORT)
+		var error = peer.connect_to_url(url, PoolStringArray(), true);
+		if error != OK:
+			print("Error connecting: ", error)
+#		peer.create_client(SERVER_IP,SERVER_PORT)
 	get_tree().network_peer = peer
 
 func _player_connected(id):
-	print("Connected")
 	if not is_network_master():
 		return
-	print("is_network_master")
+	print("Sending top scores to:", id)
 	rset_id(id, "top_scores", top_scores)
 
 master func submit_score(player_name, best_time, difficulty):
-	print("Got a submit_score from: ", player_name, " ", best_time)
-	top_scores.append({"N":player_name, "T":best_time})
-
 	var sender_id = get_tree().get_rpc_sender_id()
+	print(sender_id, " submit score: ", player_name, " ", best_time)
+	top_scores.append({"N":player_name, "T":best_time})
 	rset_id(sender_id, "top_scores", top_scores)
+
+func _process(delta):
+	peer.poll()
