985265b50605e609f751eea9a32113fc2a098020
Kameronbsmiley <87618192+Kameronbsmiley@users.noreply.github.com>
2021-07-23 23:52:36 -0400

    Menu DONE with title!!!!
    small change to world for balance

--- GeoJam 2021/SCRIPTS/TutorialScreen.gd
+++ GeoJam 2021/SCRIPTS/TutorialScreen.gd
@@ -5,16 +5,18 @@ onready var basic_cube = $Control/BasicCube
 onready var bouncy_circle = $Control/BouncyCircle
 onready var dash_triangle = $Control/DashTriangle
 onready var big_square = $Control/BigSquare
+onready var basic_controls = $Control/BasicControls
+onready var play_button = $Play
 
 var current_page = 0
 
 func _ready():
 	yield(get_tree().create_timer(1),"timeout")
 	$AnimationPlayer.play_backwards("Transition")
-
+	play_button.visible = false
 
 func _process(delta):
-	current_page = clamp(current_page, 0, 4)
+	current_page = clamp(current_page, 0, 5)
 
 	match current_page:
 
@@ -23,11 +25,13 @@ func _process(delta):
 			bouncy_circle.visible = false
 			dash_triangle.visible = false
 			big_square.visible = false
-			
+			basic_controls.visible = true
 
 		1:
+			
 			basic_cube.visible = true
 			bouncy_circle.visible = false
+			basic_controls.visible = false
 
 		2:
 			basic_cube.visible = false
@@ -42,7 +46,10 @@ func _process(delta):
 		4:
 			dash_triangle.visible = false
 			big_square.visible = true
-	
+			play_button.visible = false
+		5:
+			big_square.visible = false
+			play_button.visible = true
 
 func _on_Next_pressed():
 	current_page += 1
@@ -50,3 +57,12 @@ func _on_Next_pressed():
 
 func _on_Previous_pressed():
 	current_page -= 1
+
+
+func _on_Skip_pressed():
+	get_tree().change_scene("res://SCENES/World.tscn")
+
+
+func _on_Play_pressed():
+	get_tree().change_scene("res://SCENES/World.tscn")
+
